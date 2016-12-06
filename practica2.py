b7b79b17884e48715383e57fd14f63a837789292
CISCO <jr198023@gmail.com>
2016-12-05 21:40:27 -0500

    practicas de hoy 05/12/2016

---
+++ practica2.py
@@ -0,0 +1,33 @@
+# es la ruta de instalacion de python esto del shebang
+#!/usr/local/bin/python
+
+print ("------- TIPO DE DATOS -----------")
+
+#tipo de datos ---> Para comentarios el #
+
+# ctrl + / par ponerle a todas las lineas los comentarios..
+
+print(type (10))
+print(type (5.5))
+print(type (1+5j))
+
+print (2+4)
+print (8**3)
+print((1+5j) + (1+8j))
+
+print ("Esto es una cedena")
+print ('Esto es una cadena con comilla simple')
+print("""
+linea 1
+linea 2
+linea 3
+""")
+
+print(type("esto es una cadena"))
+
+print("cadena 1" + "cadena 2")
+
+print("cadena 1" * 2)
+
+print(type(True))
+print(type(False))
---
+++ practica3.py
@@ -0,0 +1,45 @@
+a=5
+print (a)
+print (type(a))
+a="cinco"
+print (a)
+print (type(a))
+
+Nombre = "José"
+print (Nombre)
+año ="2016"
+print(año)
+
+
+
+print (3 & 2)
+print (3 << 2)
+
+a = 10
+#a = a +9
+a+=9
+print (a)
+
+
+# OPERADORES LOGICOS AND OR NOT
+
+nombre1 = "José"
+nombre2 = "José"
+print(nombre1 == nombre2)
+
+nombre1 = "José"
+nombre2 = "Jose"
+print(nombre1 == nombre2)
+
+anio_nac = 1986
+print ("Mayor de edad")
+print((2016 - anio_nac) >= 18)
+
+edad = 60
+print("Adolecente ?? 12 - 18 ")
+print( edad >=12 and edad <= 18)
+print( 12 <= edad <= 18)
+
+
+print ("edad no laboral ??")
+print( 1 <= edad <= 18 or 80 <= edad <= 120)

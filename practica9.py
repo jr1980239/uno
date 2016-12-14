dbeecb40a433847fe9da5a7b91d5ee3ce4085577
CISCO <jr198023@gmail.com>
2016-12-14 18:25:08 -0500

    practicas de hoy 05/12/2016

--- practica9.py
+++ practica9.py
@@ -2,4 +2,4 @@ from ubidots import ApiClient
 
 api = ApiClient(token='jVYIpUpCxX12EVkSqDFAxWcAopdBf1')
 my_variable = api.get_variable('5848c8d87625422d640ad3e2')
-new_value = my_variable.save_value({'value': 30})
+new_value = my_variable.save_value({'value': 50})

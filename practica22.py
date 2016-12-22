3baac91047cad97c82fb8137a7021a3c585aa4f2
CISCO <jr198023@gmail.com>
2016-12-21 21:09:55 -0500

    practicas de hoy 05/12/2016

--- practica22.py
+++ practica22.py
@@ -9,8 +9,35 @@ print(d[1,0])
 print(d[1,1])
 print(d[5:4])
 
-"""
+
 
 d[3]="Nuevo elemento"
 d[1]="Reemplazo de valor"
+del d[1.1]
+
 print(d)
+
+
+print(d.keys())
+print(d.values())
+print(d.items())
+
+# recorrer un diccionario
+for k in d.keys():
+    print(k, "\t", d[k])
+
+for k,v in d.items():
+    print(k, "\t", v)
+
+for k in d:
+  print(k, "\t", d[k])
+
+a= (1,0)
+if a in d:
+    print(d[a])
+"""
+k=1
+if k in d:
+    print(d[k])
+else:
+    print("La clave no existe")
